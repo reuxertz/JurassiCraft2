b206524f16532cd054c3b1dbb14dedd2fff7d54d
Wyn Price <wynprice999@gmail.com>
2018-05-13 00:22:32 +0100

    Migrated genesis to reuxertz/Genesis

--- src/main/java/org/genesis/api/IGenesisRegistry.java
+++
@@ -1,9 +0,0 @@
-package org.genesis.api;
-/**
- * The main Registry Interface thats passed into custom classes to be used to register pretty much everything Genesis related.
- * @author Wyn Price
- *
- */
-public interface IGenesisRegistry {
-
-}
