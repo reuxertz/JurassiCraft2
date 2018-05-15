a35b95780ac84d1220a4cbb1e1c1ae6f7e74c5ab
Wyn Price <wynprice999@gmail.com>
2018-05-15 19:24:23 +0100

    Added car animations from 1.12.2 branch

--- src/main/java/org/jurassicraft/client/render/entity/FordExplorerRenderer.java
+++ src/main/java/org/jurassicraft/client/render/entity/FordExplorerRenderer.java
@@ -2,11 +2,13 @@ package org.jurassicraft.client.render.entity;
 
 import org.jurassicraft.JurassiCraft;
 import org.jurassicraft.client.model.ResetControlTabulaModel;
-import org.jurassicraft.server.entity.ai.util.MathUtils;
+import org.jurassicraft.client.model.animation.entity.vehicle.CarAnimator;
+import org.jurassicraft.client.model.animation.entity.vehicle.CarAnimator.Door;
 import org.jurassicraft.server.entity.vehicle.CarEntity;
 import org.jurassicraft.server.entity.vehicle.FordExplorerEntity;
 import org.jurassicraft.server.tabula.TabulaModelHelper;
 
+import net.ilexiconn.llibrary.client.model.tabula.ITabulaModelAnimator;
 import net.ilexiconn.llibrary.client.model.tabula.TabulaModel;
 import net.ilexiconn.llibrary.client.model.tabula.container.TabulaModelContainer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -14,7 +16,6 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -22,16 +23,21 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class FordExplorerRenderer extends Render<FordExplorerEntity> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(JurassiCraft.MODID, "textures/entities/ford_explorer/ford_explorer.png");
     protected static final ResourceLocation[] DESTROY_STAGES = new ResourceLocation[] { new ResourceLocation("textures/blocks/destroy_stage_0.png"), new ResourceLocation("textures/blocks/destroy_stage_1.png"), new ResourceLocation("textures/blocks/destroy_stage_2.png"), new ResourceLocation("textures/blocks/destroy_stage_3.png"), new ResourceLocation("textures/blocks/destroy_stage_4.png"), new ResourceLocation("textures/blocks/destroy_stage_5.png"), new ResourceLocation("textures/blocks/destroy_stage_6.png"), new ResourceLocation("textures/blocks/destroy_stage_7.png"), new ResourceLocation("textures/blocks/destroy_stage_8.png"), new ResourceLocation("textures/blocks/destroy_stage_9.png") };
-
+    
+    private CarAnimator animator;
     private TabulaModel baseModel;
 
     public FordExplorerRenderer(RenderManager manager) {
         super(manager);
 
         try {
+            this.animator = new CarAnimator()
+        	    .addDoor(new Door("door left main", 0, true))
+        	    .addDoor(new Door("door right main", 1, false))
+        	    .addDoor(new Door("Back door left main", 2, true))
+        	    .addDoor(new Door("Back door right main", 3, false));
             TabulaModelContainer container = TabulaModelHelper.loadTabulaModel("/assets/jurassicraft/models/entities/ford_explorer/ford_explorer.tbl");
-
-            this.baseModel = new ResetControlTabulaModel(container);
+            this.baseModel = new ResetControlTabulaModel(container, (ITabulaModelAnimator) animator);
         } catch (Exception e) {
             JurassiCraft.INSTANCE.getLogger().fatal("Failed to load the models for the Ford Explorer", e);
         }
@@ -39,6 +45,7 @@ public class FordExplorerRenderer extends Render<FordExplorerEntity> {
 
     @Override
     public void doRender(FordExplorerEntity entity, double x, double y, double z, float yaw, float partialTicks) {
+	animator.partialTicks = partialTicks;
         GlStateManager.enableBlend();
         GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
         this.bindEntityTexture(entity);
@@ -64,15 +71,8 @@ public class FordExplorerRenderer extends Render<FordExplorerEntity> {
     private void renderModel(FordExplorerEntity entity, double x, double y, double z, float yaw, float pitch, float partialTicks) {
         GlStateManager.pushMatrix();
         GlStateManager.translate((float) x, (float) y + 1.25F, (float) z);
-        
-        double d0 = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * (double)partialTicks;
-        double d1 = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * (double)partialTicks;
-        double d2 = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * (double)partialTicks;
-        float f3 = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks; //TODO: interp ?
-
         GlStateManager.rotate(180 - yaw, 0, 1, 0);
         CarRenderer.doCarRotations(entity, partialTicks);
-       
         GlStateManager.scale(-1.0F, -1.0F, 1.0F);
         this.baseModel.render(entity, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
         GlStateManager.popMatrix();
