37625581e7785ffad4938e8747494261ad5e6934
Wyn Price <wynprice999@gmail.com>
2018-05-17 16:37:18 +0100

    Improved the way tyre tracks are rendered (from 1.12.2 branch)

--- src/main/java/org/jurassicraft/server/entity/vehicle/util/CarWheel.java
+++ src/main/java/org/jurassicraft/server/entity/vehicle/util/CarWheel.java
@@ -8,9 +8,14 @@ public class CarWheel {
     
     private final Vector2d relativeWheelPosition;        
     private Vec3d currentWheelPos = Vec3d.ZERO;
+    private Vec3d prevCurrentWheelPos = Vec3d.ZERO;
+    private final int ID;
     
-    public CarWheel(Vector2d relativeWheelPosition) {
+    private CarWheel oppositeWheel;
+    
+    public CarWheel(int id, Vector2d relativeWheelPosition) {
 	this.relativeWheelPosition = relativeWheelPosition;
+	this.ID = id;
     }
     
     public Vector2d getRelativeWheelPosition() {
@@ -18,10 +23,28 @@ public class CarWheel {
     }
     
     public void setCurrentWheelPos(Vec3d currentWheelPos) {
+	this.prevCurrentWheelPos = this.currentWheelPos;
         this.currentWheelPos = currentWheelPos;
+    }
+    
+    public Vec3d getPrevCurrentWheelPos() {
+	return prevCurrentWheelPos;
     }
     
     public Vec3d getCurrentWheelPos() {
         return currentWheelPos;
+    }
+    
+    public int getID() {
+	return ID;
+    }
+    
+    public void setPair(CarWheel oppositeWheel) {
+	this.oppositeWheel = oppositeWheel;
+	oppositeWheel.oppositeWheel = this;
+    }
+    
+    public CarWheel getOppositeWheel() {
+	return oppositeWheel;
     }
 }
