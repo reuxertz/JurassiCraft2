c60291dc2045b129be85b819d28ebf8673138ef5
Wyn Price <wynprice999@gmail.com>
2018-06-03 03:17:54 +0100

    Centralized car rendering code from 1.12 branch

--- src/main/java/org/jurassicraft/client/render/entity/CarRenderer.java
+++ src/main/java/org/jurassicraft/client/render/entity/CarRenderer.java
@@ -2,10 +2,14 @@ package org.jurassicraft.client.render.entity;
 
 import java.util.stream.IntStream;
 
+import javax.annotation.Nullable;
 import javax.vecmath.Vector2d;
 import javax.vecmath.Vector4d;
 
+import net.ilexiconn.llibrary.client.model.tabula.TabulaModel;
+import net.ilexiconn.llibrary.client.model.tabula.container.TabulaModelContainer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -13,8 +17,12 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import org.jurassicraft.JurassiCraft;
+import org.jurassicraft.client.model.TabulaModelUV;
+import org.jurassicraft.client.model.animation.entity.vehicle.CarAnimator;
 import org.jurassicraft.server.entity.ai.util.MathUtils;
 import org.jurassicraft.server.entity.vehicle.CarEntity;
+import org.jurassicraft.server.tabula.TabulaModelHelper;
 
 @SideOnly(Side.CLIENT)
 public abstract class CarRenderer<E extends CarEntity> extends Render<E> {
@@ -22,10 +30,61 @@ public abstract class CarRenderer<E extends CarEntity> extends Render<E> {
         .mapToObj(n -> new ResourceLocation(String.format("textures/blocks/destroy_stage_%d.png", n)))
         .toArray(ResourceLocation[]::new);
 
-    protected CarRenderer(RenderManager renderManager) {
+    protected final String carName;
+    protected final ResourceLocation texture;
+
+    protected CarAnimator animator;
+    protected TabulaModel baseModel;
+    protected TabulaModel destroyModel;
+
+    protected CarRenderer(RenderManager renderManager, String carName) {
         super(renderManager);
+        this.carName = carName;
+        this.animator = createCarAnimator();
+        texture = new ResourceLocation(JurassiCraft.MODID, "textures/entities/" + carName + "/" + carName + ".png");
+        try {
+            TabulaModelContainer container = TabulaModelHelper.loadTabulaModel("/assets/jurassicraft/models/entities/" + carName + "/" + carName + ".tbl");
+            this.baseModel = new TabulaModel(container, animator);
+            this.destroyModel = new TabulaModel(new TabulaModelUV(container, 16, 16), animator);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load car " + carName,e);
+        }
+
     }
-    
+
+    @Override
+    public void doRender(E entity, double x, double y, double z, float yaw, float partialTicks) {
+        this.animator.partialTicks = partialTicks;
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        this.bindEntityTexture(entity);
+        this.renderModel(entity, x, y, z, yaw, partialTicks, false);
+        int destroyStage = Math.min(10, (int) (10 - (entity.getHealth() / CarEntity.MAX_HEALTH) * 10)) - 1;
+        if (destroyStage >= 0) {
+            GlStateManager.color(1, 1, 1, 0.5F);
+            GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.DST_COLOR, GlStateManager.DestFactor.SRC_COLOR, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+            GlStateManager.doPolygonOffset(-3, -3);
+            GlStateManager.enablePolygonOffset();
+            RenderHelper.disableStandardItemLighting();
+            this.bindTexture(DESTROY_STAGES[destroyStage]);
+            this.renderModel(entity, x, y, z, yaw, partialTicks, true);
+            GlStateManager.doPolygonOffset(0, 0);
+            GlStateManager.disablePolygonOffset();
+            RenderHelper.enableStandardItemLighting();
+        }
+        GlStateManager.disableBlend();
+        super.doRender(entity, x, y, z, yaw, partialTicks);
+    }
+
+    protected void renderModel(E entity, double x, double y, double z, float yaw, float partialTicks, boolean destroy) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate((float) x, (float) y + 1.25F, (float) z);
+        GlStateManager.rotate(180 - yaw, 0, 1, 0);
+        this.doCarRotations(entity, partialTicks);
+        GlStateManager.scale(-1, -1, 1);
+        (destroy ? this.destroyModel : this.baseModel).render(entity, 0, 0, 0, 0, 0, 0.0625F);
+        GlStateManager.popMatrix();
+    }
     public static void doCarRotations(CarEntity entity, float partialTicks) {
 	double backValue = entity.backValue.getValueForRendering(partialTicks);
         double frontValue = entity.frontValue.getValueForRendering(partialTicks);
@@ -41,5 +100,13 @@ public abstract class CarRenderer<E extends CarEntity> extends Render<E> {
         GlStateManager.translate(0, -rot.x, -rot.y);
         float localRotationRoll = (float) MathUtils.cosineFromPoints(new Vec3d(rightValue, 0, vec.z), new Vec3d(leftValue, 0, vec.z), new Vec3d(leftValue, 0, vec.x));//TODO: same as above
         GlStateManager.rotate(leftValue < rightValue ? localRotationRoll : -localRotationRoll, 0, 0, 1);
+    }
+
+    protected abstract CarAnimator createCarAnimator();
+
+    @Nullable
+    @Override
+    protected ResourceLocation getEntityTexture(E entity) {
+        return this.texture;
     }
 }
