85f6f446ac86b7bab6dbfb2da924aad46317e87b
Wyn Price <wynprice999@gmail.com>
2018-06-03 03:38:41 +0100

    Fixed issues with doors opening for entities inside vehicles

--- src/main/java/org/jurassicraft/client/model/animation/entity/vehicle/CarAnimator.java
+++ src/main/java/org/jurassicraft/client/model/animation/entity/vehicle/CarAnimator.java
@@ -36,7 +36,7 @@ public class CarAnimator implements ITabulaModelAnimator<CarEntity> {
 					closestSeat = door1.getSeat(entity);
 				}
 			}
-			value.setTarget(Math.toRadians(seat.getOccupant() != null || closestSeat != seat || closestSeat.getPos().distanceTo(playerPos) > 4D ? 0F : door.isLeft() ? 60F : -60F));
+			value.setTarget(Math.toRadians(entity.getPassengers().contains(player) || seat.getOccupant() != null || closestSeat != seat || closestSeat.getPos().distanceTo(playerPos) > 4D ? 0F : door.isLeft() ? 60F : -60F));
 			model.getCube(door.getName()).rotateAngleY = (float) value.getValueForRendering(partialTicks);
 		});
 
