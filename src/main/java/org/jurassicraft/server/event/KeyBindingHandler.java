6077b1e5ca5c4bb141bcb7c341f4a74f4d308045
Wyn Price <wynprice999@gmail.com>
2018-06-03 03:39:07 +0100

    Move key binding code to correct package

--- src/main/java/org/jurassicraft/server/event/KeyBindingHandler.java
+++ src/main/java/org/jurassicraft/server/event/KeyBindingHandler.java
@@ -1,19 +1,25 @@
-package org.jurassicraft.server.command;
+package org.jurassicraft.server.event;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.settings.IKeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jurassicraft.server.entity.vehicle.CarEntity;
 import org.lwjgl.input.Keyboard;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @SideOnly(Side.CLIENT)
 public class KeyBindingHandler {
     public static KeyBinding MICRORAPTOR_DISMOUNT = new KeyBinding("key.microraptor_dismount", Keyboard.KEY_C, "JurassiCraft");
-    public static KeyBinding VEHICLE_SWITCH_SEAT_CONTROL = new KeyBinding("key.vehicle.switch", new IKeyConflictContext() {
+
+    private static IKeyConflictContext context = new IKeyConflictContext() {
         @Override
         public boolean isActive() {
             EntityPlayer player = Minecraft.getMinecraft().player;
@@ -22,13 +28,16 @@ public class KeyBindingHandler {
 
         @Override
         public boolean conflicts(IKeyConflictContext other) {
-            return false;
+            return true;
         }
-    }, Keyboard.KEY_LCONTROL, "JurassiCraft");
+    };
+
+    public static List<KeyBinding> VEHICLE_KEY_BINDINGS = IntStream.range(0, 9)
+            .mapToObj(i -> new KeyBinding("key.vehicle_" + i + ".switch", context, KeyModifier.CONTROL, Keyboard.KEY_1 + i, "JurassiCraft"))
+            .collect(Collectors.toList());
 
     public static void init() {
         ClientRegistry.registerKeyBinding(MICRORAPTOR_DISMOUNT);
-        ClientRegistry.registerKeyBinding(VEHICLE_SWITCH_SEAT_CONTROL);
-
+        VEHICLE_KEY_BINDINGS.forEach(ClientRegistry::registerKeyBinding);
     }
 }
