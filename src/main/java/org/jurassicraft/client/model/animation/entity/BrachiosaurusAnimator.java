d611fd611e616baa3c2e8b21183ce1e6e514c3c5
Wyn Price <wynprice999@gmail.com>
2018-06-12 02:02:55 +0100

    Fixed #293

--- src/main/java/org/jurassicraft/client/model/animation/entity/BrachiosaurusAnimator.java
+++ src/main/java/org/jurassicraft/client/model/animation/entity/BrachiosaurusAnimator.java
@@ -1,4 +1,3 @@
-
 package org.jurassicraft.client.model.animation.entity;
 
 import net.ilexiconn.llibrary.client.model.tools.AdvancedModelRenderer;
@@ -14,44 +13,47 @@ public class BrachiosaurusAnimator extends EntityAnimator<BrachiosaurusEntity> {
     @Override
     protected void performAnimations(AnimatableModel model, BrachiosaurusEntity entity, float f, float f1, float ticks, float rotationYaw, float rotationPitch, float scale) {
         AdvancedModelRenderer head = model.getCube("head");
-        AdvancedModelRenderer neck1 = model.getCube("neck1");
+
+        AdvancedModelRenderer neck1 = model.getCube("Neck 1");
         AdvancedModelRenderer neck2 = model.getCube("neck2");
         AdvancedModelRenderer neck3 = model.getCube("neck3");
         AdvancedModelRenderer neck4 = model.getCube("neck4");
         AdvancedModelRenderer neck5 = model.getCube("neck5");
         AdvancedModelRenderer neck6 = model.getCube("neck6");
+
         AdvancedModelRenderer neck7 = model.getCube("neck7");
-        AdvancedModelRenderer neck8 = model.getCube("neck8");
-        AdvancedModelRenderer neck9 = model.getCube("neck9");
-        AdvancedModelRenderer neck10 = model.getCube("neck10");
         AdvancedModelRenderer tail1 = model.getCube("tail1");
         AdvancedModelRenderer tail2 = model.getCube("tail2");
         AdvancedModelRenderer tail3 = model.getCube("tail3");
         AdvancedModelRenderer tail4 = model.getCube("tail4");
         AdvancedModelRenderer tail5 = model.getCube("tail5");
-        AdvancedModelRenderer tail6 = model.getCube("tail6");
-        AdvancedModelRenderer[] neckParts = new AdvancedModelRenderer[] { head, neck10, neck9, neck8, neck7, neck6, neck5, neck4, neck3, neck2, neck1 };
-        AdvancedModelRenderer[] tailParts = new AdvancedModelRenderer[] { tail6, tail5, tail4, tail3 };
-        AdvancedModelRenderer[] tailParts2 = new AdvancedModelRenderer[] { tail6, tail5, tail4, tail3, tail2, tail1 };
+
+        AdvancedModelRenderer[] neckParts = new AdvancedModelRenderer[] { head, neck7, neck6, neck5, neck4, neck3, neck2, neck1 };
+        AdvancedModelRenderer[] tailParts = new AdvancedModelRenderer[] { tail5, tail4, tail3 };
+        AdvancedModelRenderer[] tailParts2 = new AdvancedModelRenderer[] { tail5, tail4, tail3, tail2, tail1 };
+
         float delta = Minecraft.getMinecraft().getRenderPartialTicks();
-        AdvancedModelRenderer root = model.getCube("bodyHips");
-        AdvancedModelRenderer backLeftThigh = model.getCube("thighLeft");
-        AdvancedModelRenderer backLeftCalf = model.getCube("calfLeft");
-        AdvancedModelRenderer backRightThigh = model.getCube("thighRight");
-        AdvancedModelRenderer backRightCalf = model.getCube("calfRight");
-        AdvancedModelRenderer frontLeftThigh = model.getCube("bicepLeft");
-        AdvancedModelRenderer frontLeftCalf = model.getCube("forearmLeft");
-        AdvancedModelRenderer frontRightThigh = model.getCube("bicepRight");
-        AdvancedModelRenderer frontRightCalf = model.getCube("forearmRight");
+        AdvancedModelRenderer root = model.getCube("hips");
+        AdvancedModelRenderer backLeftThigh = model.getCube("top leg left");
+        AdvancedModelRenderer backLeftCalf = model.getCube("bottom leg left");
+        AdvancedModelRenderer backRightThigh = model.getCube("top leg right");
+        AdvancedModelRenderer backRightCalf = model.getCube("bottom leg right");
+        AdvancedModelRenderer frontLeftThigh = model.getCube("front left top leg");
+        AdvancedModelRenderer frontLeftCalf = model.getCube("bottom front left leg");
+        AdvancedModelRenderer frontRightThigh = model.getCube("front right top leg");
+        AdvancedModelRenderer frontRightCalf = model.getCube("bottom front right leg");
         LegArticulator.articulateQuadruped(entity, entity.legSolver, root, neck1,
-            backLeftThigh, backLeftCalf, backRightThigh, backRightCalf, frontLeftThigh, frontLeftCalf, frontRightThigh, frontRightCalf,
-            0.25F, 0.4F, -0.2F, -0.3F,
-            delta
+                backLeftThigh, backLeftCalf, backRightThigh, backRightCalf, frontLeftThigh, frontLeftCalf, frontRightThigh, frontRightCalf,
+                0.25F, 0.4F, -0.2F, -0.3F,
+                delta
         );
+
         float globalSpeed = 0.4F;
         float globalHeight = 0.5F;
+
         model.chainWave(tailParts, globalSpeed * 0.25F, globalHeight * 1.0F, 3, ticks, 0.025F);
         model.chainWave(neckParts, globalSpeed * 0.25F, globalHeight * 0.25F, -3, ticks, 0.025F);
+
         entity.tailBuffer.applyChainSwingBuffer(tailParts2);
     }
 }
--- src/main/java/org/jurassicraft/client/model/animation/entity/TyrannosaurusAnimator.java
+++ src/main/java/org/jurassicraft/client/model/animation/entity/TyrannosaurusAnimator.java
@@ -1,59 +1,55 @@
 package org.jurassicraft.client.model.animation.entity;
 
+import org.jurassicraft.client.model.AnimatableModel;
+import org.jurassicraft.client.model.animation.EntityAnimator;
+import org.jurassicraft.server.entity.dinosaur.TyrannosaurusEntity;
+
 import net.ilexiconn.llibrary.client.model.tools.AdvancedModelRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.jurassicraft.client.model.AnimatableModel;
-import org.jurassicraft.client.model.animation.EntityAnimator;
-import org.jurassicraft.server.entity.dinosaur.TyrannosaurusEntity;
 
 @SideOnly(Side.CLIENT)
 public class TyrannosaurusAnimator extends EntityAnimator<TyrannosaurusEntity> {
     @Override
     protected void performAnimations(AnimatableModel model, TyrannosaurusEntity entity, float f, float f1, float ticks, float rotationYaw, float rotationPitch, float scale) {
-        AdvancedModelRenderer waist = model.getCube("bodyHips");
-        AdvancedModelRenderer stomach = model.getCube("bodyBack");
-        AdvancedModelRenderer chest = model.getCube("bodyShoulders");
+        AdvancedModelRenderer waist = model.getCube("Body 1");
+        AdvancedModelRenderer stomach = model.getCube("Body 2");
+        AdvancedModelRenderer chest = model.getCube("Body 3");
 
-        AdvancedModelRenderer neck1 = model.getCube("neck1");
-        AdvancedModelRenderer neck2 = model.getCube("neck2");
-        AdvancedModelRenderer neck3 = model.getCube("neck3");
-        AdvancedModelRenderer neck4 = model.getCube("neck4");
-        AdvancedModelRenderer neck5 = model.getCube("neck5");
-        AdvancedModelRenderer neck6 = model.getCube("neck6");
-        AdvancedModelRenderer neck7 = model.getCube("neck7");
-        AdvancedModelRenderer neck8 = model.getCube("neck8");
+        AdvancedModelRenderer neck1 = model.getCube("Neck1");
+        AdvancedModelRenderer neck2 = model.getCube("Neck2");
+        AdvancedModelRenderer neck3 = model.getCube("Neck3");
+        AdvancedModelRenderer neck4 = model.getCube("Neck4");
+        AdvancedModelRenderer neck5 = model.getCube("Neck5");
 
-        AdvancedModelRenderer head = model.getCube("head");
+        AdvancedModelRenderer head = model.getCube("Head");
 
-        AdvancedModelRenderer tail1 = model.getCube("tail1");
-        AdvancedModelRenderer tail2 = model.getCube("tail2");
-        AdvancedModelRenderer tail3 = model.getCube("tail3");
-        AdvancedModelRenderer tail4 = model.getCube("tail4");
-        AdvancedModelRenderer tail5 = model.getCube("tail5");
-        AdvancedModelRenderer tail6 = model.getCube("tail6");
-        AdvancedModelRenderer tail7 = model.getCube("tail7");
-        AdvancedModelRenderer tail8 = model.getCube("tail8");
-        AdvancedModelRenderer tail9 = model.getCube("tail9");
+        AdvancedModelRenderer tail1 = model.getCube("Tail 1");
+        AdvancedModelRenderer tail2 = model.getCube("Tail 2");
+        AdvancedModelRenderer tail3 = model.getCube("Tail 3");
+        AdvancedModelRenderer tail4 = model.getCube("Tail 4");
+        AdvancedModelRenderer tail5 = model.getCube("Tail 5");
+        AdvancedModelRenderer tail6 = model.getCube("Tail 6");
+        AdvancedModelRenderer tail7 = model.getCube("Tail 7");
 
-        AdvancedModelRenderer handLeft = model.getCube("handLeft");
-        AdvancedModelRenderer lowerArmLeft = model.getCube("forearmLeft");
+        AdvancedModelRenderer handLeft = model.getCube("Hand LEFT");
+        AdvancedModelRenderer lowerArmLeft = model.getCube("Lower Arm LEFT");
 
-        AdvancedModelRenderer handRight = model.getCube("handRight");
-        AdvancedModelRenderer lowerArmRight = model.getCube("forearmRight");
+        AdvancedModelRenderer handRight = model.getCube("Hand Right");
+        AdvancedModelRenderer lowerArmRight = model.getCube("Lower Arm Right");
 
-        AdvancedModelRenderer leftThigh = model.getCube("thighLeft");
-        AdvancedModelRenderer rightThigh = model.getCube("thighRight");
+        AdvancedModelRenderer leftThigh = model.getCube("Left Thigh");
+        AdvancedModelRenderer rightThigh = model.getCube("Right Thigh");
 
-        AdvancedModelRenderer[] tailParts = new AdvancedModelRenderer[] { tail9, tail8, tail7, tail6, tail5, tail4, tail3, tail2, tail1 };
-        AdvancedModelRenderer[] bodyParts = new AdvancedModelRenderer[] { head, neck8, neck7, neck6, neck5, neck4, neck3, neck2, neck1, chest, stomach, waist };
+        AdvancedModelRenderer[] tailParts = new AdvancedModelRenderer[] { tail7, tail6, tail5, tail4, tail3, tail2, tail1 };
+        AdvancedModelRenderer[] bodyParts = new AdvancedModelRenderer[] { head, neck5, neck4, neck3, neck2, neck1, chest, stomach, waist };
         AdvancedModelRenderer[] leftArmParts = new AdvancedModelRenderer[] { handLeft, lowerArmLeft };
         AdvancedModelRenderer[] rightArmParts = new AdvancedModelRenderer[] { handRight, lowerArmRight };
 
         float delta = Minecraft.getMinecraft().getRenderPartialTicks();
-        AdvancedModelRenderer leftCalf = model.getCube("calfLeft");
-        AdvancedModelRenderer rightCalf = model.getCube("calfRight");
+        AdvancedModelRenderer leftCalf = model.getCube("Left Calf 1");
+        AdvancedModelRenderer rightCalf = model.getCube("Right Calf 1");
         LegArticulator.articulateBiped(entity, entity.legSolver, waist, leftThigh, leftCalf, rightThigh, rightCalf, 0.4F, 0.4F, delta);
 
         float globalSpeed = 0.5F;
