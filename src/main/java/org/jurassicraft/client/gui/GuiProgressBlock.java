367a8c2bb69f56519eb30d50c01f71201322424c
Cazsius <cazsius0518@gmail.com>
2018-07-21 05:43:26 -0400

    Fixed GUI Issue

--- src/main/java/org/jurassicraft/client/gui/GuiProgressBlock.java
+++ src/main/java/org/jurassicraft/client/gui/GuiProgressBlock.java
@@ -13,6 +13,16 @@ public class GuiProgressBlock {
     private final int texY;
     private final DoubleSupplier supplier;
 
+    public GuiProgressBlock(int x, int y, int width, int height, int texX, int texY, DoubleSupplier supplier) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.texX = texX;
+        this.texY = texY;
+        this.supplier = supplier;
+    }
+
     public void render(GuiScreen gui) {
         gui.drawTexturedModalRect(this.x, this.y, this.texX, this.texY, (int)(supplier.getAsDouble() * this.width), this.height);
     }
