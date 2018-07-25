dcca803e4f34dea1978089dc2410df8dc607a558
Cazsius <cazsius0518@gmail.com>
2018-07-24 22:41:56 -0400

    Fixed Git Commit Issue

--- src/main/java/org/jurassicraft/JurassiCraft.java
+++ src/main/java/org/jurassicraft/JurassiCraft.java
@@ -11,8 +11,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import org.apache.logging.log4j.Logger;
-import org.jurassicraft.server.command.CommandSetNBT;
 import org.jurassicraft.server.command.ForceAnimationCommand;
+import org.jurassicraft.server.command.SpawnStructureCommand;
 import org.jurassicraft.server.message.*;
 import org.jurassicraft.server.proxy.ServerProxy;
 
@@ -31,7 +31,7 @@ public class JurassiCraft {
 
     public static long timerTicks;
 
-    @NetworkWrapper({ PlacePaddockSignMessage.class, ChangeTemperatureMessage.class, HelicopterEngineMessage.class, HelicopterDirectionMessage.class, HelicopterModulesMessage.class, SwitchHybridizerCombinatorMode.class, SetOrderMessage.class, OpenFieldGuideGuiMessage.class, UpdateVehicleControlMessage.class, MicroraptorDismountMessage.class, FordExplorerChangeStateMessage.class, FordExplorerUpdatePositionStateMessage.class, DNASequenceTransferClicked.class, CultivatorSyncNutrients.class, CarEntityPlayRecord.class, AttemptMoveToSeatMessage.class, SyncTrackingTabletMap.class, StopMapSyncMessage.class})
+    @NetworkWrapper({ PlacePaddockSignMessage.class, ChangeTemperatureMessage.class, HelicopterEngineMessage.class, HelicopterDirectionMessage.class, HelicopterModulesMessage.class, SwitchHybridizerCombinatorMode.class, SetOrderMessage.class, OpenFieldGuideGuiMessage.class, UpdateVehicleControlMessage.class, MicroraptorDismountMessage.class, FordExplorerChangeStateMessage.class, FordExplorerUpdatePositionStateMessage.class, DNASequenceTransferClicked.class, CultivatorSyncNutrients.class, CarEntityPlayRecord.class, AttemptMoveToSeatMessage.class})
     public static SimpleNetworkWrapper NETWORK_WRAPPER;
 
     private static Logger logger;
@@ -55,7 +55,7 @@ public class JurassiCraft {
     @Mod.EventHandler
     public void onServerStarting(FMLServerStartingEvent event) {
         event.registerServerCommand(new ForceAnimationCommand());
-        event.registerServerCommand(new CommandSetNBT());
+        event.registerServerCommand(new SpawnStructureCommand());
     }
 
     public static Logger getLogger() {
