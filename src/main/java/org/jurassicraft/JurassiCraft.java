47b663f699d17a07db5c2f4a653ad4487b35f002
Cazsius <cazsius0518@gmail.com>
2018-07-29 17:43:02 -0400

    Updated Version Number and Changelog

--- src/main/java/org/jurassicraft/JurassiCraft.java
+++ src/main/java/org/jurassicraft/JurassiCraft.java
@@ -20,7 +20,7 @@ import org.jurassicraft.server.proxy.ServerProxy;
 public class JurassiCraft {
     public static final String MODID = "jurassicraft";
     public static final String NAME = "JurassiCraft";
-    public static final String VERSION = "2.1.12";
+    public static final String VERSION = "2.1.13 ";
 
     public static final String LLIBRARY_VERSION = "1.7.9";
     @SidedProxy(serverSide = "org.jurassicraft.server.proxy.ServerProxy", clientSide = "org.jurassicraft.client.proxy.ClientProxy")
